d0ba0c767fd07188facfe9627554ec5b8b14650f
Rafael Cerri <rafaelbcerri@gmail.com>
2019-05-16 02:04:34 -0300

    Refactor

--- app.js
+++ app.js
@@ -1,86 +1,81 @@
 const element = document.getElementsByClassName('items')[0];
 let offset = 3;
-// const hammertime = Hammer(element).on('swipe', () => {
-//   swipeGif();
-// })
 
-async function swipeGif() {
-  const data = await getGifs(1, offset);
-  const imageUrl = data[0].images.fixed_height.url;
-  element.children[element.children.length - 1].remove();
-  element.insertAdjacentHTML("afterbegin", template(imageUrl));
+async function initialize() {
+  const gifsData = await getGifs();
 
-  const card = document.querySelectorAll(".card")[0];
-  const elementHammer = new Hammer(card);
+  gifsData.forEach(item => {
+    const imageUrl = item.images.fixed_height.url;
+    element.insertAdjacentHTML("afterbegin", template(imageUrl));
+  });
+
+  const cards = document.querySelectorAll(".card");
+  cards.forEach(element => addHammerToElement(element))
+}
+
+function addHammerToElement(element) {
+  const elementHammer = new Hammer(element);
   elementHammer.on("pan", (event) => {
-    const xMulti = event.deltaX * 0.2;
-    if (event.deltaX < 100 && event.deltaX > -100) {
-      card.style.transform = 'translate(' + event.deltaX + 'px, ' + 0 + 'px) rotate(' + xMulti + 'deg)';
+    const xMovement = event.deltaX;
+    if (xMovement > 100 || xMovement < -100) {
+      const rotationDeg = xMovement * 0.2;
+
+      renderAnimation(element, xMovement, 0, rotationDeg);
     }
   });
 
   elementHammer.on("panend", event => {
-    const xMulti = event.deltaX * 0.2;
-    if (event.deltaX > 90 || event.deltaX < -90) {
-      var moveOutWidth = document.body.clientWidth;
-      var endX = Math.max(Math.abs(event.velocityX) * moveOutWidth, moveOutWidth);
-      var toX = event.deltaX > 0 ? endX : -endX;
-      var endY = Math.abs(event.velocityY) * moveOutWidth;
-      var toY = event.deltaY > 0 ? endY : -endY;
-      card.style.transform = 'translate(' + toX + 'px, ' + (toY + event.deltaY) + 'px) rotate(' + xMulti + 'deg)';
-      console.log("Manda ver")
-      swipeGif()
+    const xMovement = event.deltaX;
+    if (xMovement > 90 || xMovement < -90) {
+      const { xPosition, yPosition} = getCoordsFromEvent(event)
+      const rotationDeg = xMovement * 0.2;
+
+      renderAnimation(element, xPosition, yPosition, rotationDeg);
+      swipeGif();
     } else {
-      card.style.transform = 'translate(0px, 0px) rotate(0deg)';
-      console.log("volta")
+      renderAnimation(element, 0, 0, 0);
     }
   });
-
-  offset++;
 }
 
-async function getGifs(limit, offset) {
+function getCoordsFromEvent(event) {
+  const moveOutWidth = document.body.clientWidth;
+  const endX = Math.max(Math.abs(event.velocityX) * moveOutWidth, moveOutWidth);
+  const toX = event.deltaX > 0 ? endX : -endX;
+  const endY = Math.abs(event.velocityY) * moveOutWidth;
+  const toY = event.deltaY > 0 ? endY : -endY;
+  return {
+    xPosition: toX,
+    yPosition: toY + event.deltaY
+  }
+}
+
+function renderAnimation(element, xPosition, yPosition, degrees) {
+  element.style.transform = `
+    translate(${xPosition}px, ${yPosition}px)
+    rotate(${degrees}deg)
+  `;
+}
+
+function getGifs(limit=3, offset=0) {
   const url = `https://api.giphy.com/v1/gifs/trending?&api_key=3Y58rC1smDMY1cLt6dNyVLpZCSS12Mfv&limit=${limit}&offset=${offset}`;
+
   return fetch(url).then(response => response.json())
     .then(json => json.data)
     .catch(error => error);
 }
 
-async function initialize() {
-  const data = await getGifs(3, 0);
-  data.forEach(item => {
-    const imageUrl = item.images.fixed_height.url;
-    element.innerHTML += template(imageUrl);
-  });
-  const cards = document.querySelectorAll(".card");
-  console.log(cards)
-  cards.forEach(element => {
-    const elementHammer = new Hammer(element);
-    elementHammer.on("pan", (event) => {
-      const xMulti = event.deltaX * 0.2;
-      if (event.deltaX < 100 && event.deltaX > -100) {
-        element.style.transform = 'translate(' + event.deltaX + 'px, ' + 0 + 'px) rotate(' + xMulti + 'deg)';
-      }
-    });
 
-    elementHammer.on("panend", event => {
-      const xMulti = event.deltaX * 0.2;
-      if (event.deltaX > 90 || event.deltaX < -90) {
-        var moveOutWidth = document.body.clientWidth;
-        var endX = Math.max(Math.abs(event.velocityX) * moveOutWidth, moveOutWidth);
-        var toX = event.deltaX > 0 ? endX : -endX;
-        var endY = Math.abs(event.velocityY) * moveOutWidth;
-        var toY = event.deltaY > 0 ? endY : -endY;
-        element.style.transform = 'translate(' + toX + 'px, ' + (toY + event.deltaY) + 'px) rotate(' + xMulti + 'deg)';
-        console.log("Manda ver")
-        swipeGif()
-      } else {
-        element.style.transform = 'translate(0px, 0px) rotate(0deg)';
-        console.log("volta")
-      }
-    });
-  })
+async function swipeGif() {
+  const data = await getGifs(1, offset++);
+  const imageUrl = data[0].images.fixed_height.url;
+  element.children[element.children.length - 1].remove();
+  element.insertAdjacentHTML("afterbegin", template(imageUrl));
+
+  const card = document.querySelectorAll(".card")[0];
+  addHammerToElement(card)
 }
+
 function template(url) {
   return `
     <div class="card">
